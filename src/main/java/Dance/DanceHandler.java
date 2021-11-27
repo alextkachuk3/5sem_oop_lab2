d07c84091da81565cc26e942ad7e370e42898ee9
alextkachuk3 <tkachuk.alexey@unicyb.kiev.ua>
2021-11-28 01:15:04 +0200

    dance xml schema validator

---
+++ src/main/java/Dance/DanceHandler.java
@@ -0,0 +1,56 @@
+package com.labs.lab2.Dance;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+import org.xml.sax.helpers.DefaultHandler;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * XML schema validation
+ */
+public class DanceHandler extends DefaultHandler {
+    private List<String> errors = new ArrayList<>();
+
+    public DanceHandler() {
+        super();
+    }
+
+    private void log(String text){
+        errors.add(text);
+    }
+
+    @Override
+    public String toString() {
+        return "Dance handler errors: " + errors;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    @Override
+    public void warning(SAXParseException e){
+        log("WARNING "+getErrorAddress(e) + " " + e.getMessage());
+    }
+
+    @Override
+    public void error(SAXParseException e){
+        log("ERROR "+getErrorAddress(e) + " " + e.getMessage());
+    }
+
+    @Override
+    public void fatalError(SAXParseException e){
+        log("FATAL ERROR "+getErrorAddress(e) + " " + e.getMessage());
+    }
+
+    private String getErrorAddress(SAXParseException e){
+        return e.getLineNumber() + ":"+e.getColumnNumber();
+    }
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        errors = new ArrayList<>();
+    }
+}
